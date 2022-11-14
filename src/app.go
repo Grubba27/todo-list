ff309b3a8e32b20e6bbb274a09e868666a39ca0f
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-14 18:24:50 -0300

    Adjusted keybinds

--- src/app.go
+++ src/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
@@ -17,6 +18,62 @@ const (
 )
 const InitWidth = 300
 const InitHeight = 300
+
+type KeyMap struct {
+	Quit     key.Binding
+	Up       key.Binding
+	Down     key.Binding
+	Left     key.Binding
+	Right    key.Binding
+	Create   key.Binding
+	Filter   key.Binding
+	Edit     key.Binding
+	Delete   key.Binding
+	MoveNext key.Binding
+}
+
+var DefaultKeyMap = KeyMap{
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+	Up: key.NewBinding(
+		key.WithKeys("k", "up"),
+		key.WithHelp("↑/k", "up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("j", "down"),
+		key.WithHelp("↓/j", "down"),
+	),
+	Right: key.NewBinding(
+		key.WithKeys("l", "right"),
+		key.WithHelp("→/l", "right"),
+	),
+	Left: key.NewBinding(
+		key.WithKeys("h", "left"),
+		key.WithHelp("←/h", "left"),
+	),
+	Create: key.NewBinding(
+		key.WithKeys("n"),
+		key.WithHelp("n", "new"),
+	),
+	Filter: key.NewBinding(
+		key.WithKeys("/"),
+		key.WithHelp("/", "filter"),
+	),
+	Edit: key.NewBinding(
+		key.WithKeys("e"),
+		key.WithHelp("e", "edit"),
+	),
+	Delete: key.NewBinding(
+		key.WithKeys("d"),
+		key.WithHelp("d", "delete"),
+	),
+	MoveNext: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("↩", "move next"),
+	),
+}
 
 var (
 	columnStyle = lipgloss.NewStyle().
@@ -110,6 +167,81 @@ func (m *App) initList(width, height int, finished func()) {
 		width/t.Padding,
 		height/2,
 	)
+	defaultList.KeyMap = list.KeyMap{
+		CursorUp: key.NewBinding(
+			key.WithKeys("up", "k"),
+			key.WithHelp("↑/k", "up"),
+		),
+		CursorDown: key.NewBinding(
+			key.WithKeys("down", "j"),
+			key.WithHelp("↓/j", "down"),
+		),
+		PrevPage: key.NewBinding(
+			key.WithKeys("left", "h", "pgup", "b", "u"),
+			key.WithHelp("←/h/pgup", "prev page"),
+		),
+		NextPage: key.NewBinding(
+			key.WithKeys("right", "l", "pgdown", "f", "d"),
+			key.WithHelp("→/l/pgdn", "next page"),
+		),
+		GoToStart: key.NewBinding(
+			key.WithKeys("home", "g"),
+			key.WithHelp("g/home", "go to start"),
+		),
+		GoToEnd: key.NewBinding(
+			key.WithKeys("end", "G"),
+			key.WithHelp("G/end", "go to end"),
+		),
+		Filter: key.NewBinding(
+			key.WithKeys("/"),
+			key.WithHelp("/", "filter"),
+		),
+		ClearFilter: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "clear filter"),
+		),
+
+		// Filtering.
+		CancelWhileFiltering: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "cancel"),
+		),
+		AcceptWhileFiltering: key.NewBinding(
+			key.WithKeys("enter", "tab", "shift+tab", "ctrl+k", "up", "ctrl+j", "down"),
+			key.WithHelp("enter", "apply filter"),
+		),
+		ShowFullHelp: key.NewBinding(
+			key.WithKeys("?"),
+			key.WithHelp("?", "more"),
+		),
+		CloseFullHelp: key.NewBinding(
+			key.WithKeys("?"),
+			key.WithHelp("?", "close help"),
+		),
+
+		// Quitting.
+		Quit: key.NewBinding(
+			key.WithKeys("q", "esc"),
+			key.WithHelp("q", "quit"),
+		),
+		ForceQuit: key.NewBinding(key.WithKeys("ctrl+c")),
+	}
+	extraKeys := []key.Binding{
+		DefaultKeyMap.Quit,
+		DefaultKeyMap.Up,
+		DefaultKeyMap.Down,
+		DefaultKeyMap.Left,
+		DefaultKeyMap.Right,
+		DefaultKeyMap.Create,
+		DefaultKeyMap.Filter,
+		DefaultKeyMap.Edit,
+		DefaultKeyMap.Delete,
+		DefaultKeyMap.MoveNext,
+	}
+	defaultList.AdditionalShortHelpKeys = func() []key.Binding {
+		return extraKeys
+	}
+	defaultList.SetShowHelp(false)
 	m.lists = []list.Model{defaultList, defaultList, defaultList}
 
 	m.lists[t.Todo].Title = "To Do"
@@ -147,30 +279,37 @@ func (m *App) View() string {
 		return fmt.Sprintf("\n\n  %s  loading... \n\n", m.Spinner.View())
 	}
 
-	todoView := m.lists[t.Todo].View()
-	inProgress := m.lists[t.InProgress].View()
-	done := m.lists[t.Done].View()
+	todoView := m.lists[t.Todo]
+	inProgress := m.lists[t.InProgress]
+	done := m.lists[t.Done]
+
+	todoView.SetShowHelp(false)
+	inProgress.SetShowHelp(false)
+	done.SetShowHelp(false)
 	switch m.focused {
 	case t.Done:
+		done.SetShowHelp(true)
 		return lipgloss.JoinHorizontal(
 			lipgloss.Left,
-			columnStyle.Render(todoView),
-			columnStyle.Render(inProgress),
-			focusedStyle.Render(done),
+			columnStyle.Render(todoView.View()),
+			columnStyle.Render(inProgress.View()),
+			focusedStyle.Render(done.View()),
 		)
 	case t.InProgress:
+		inProgress.SetShowHelp(true)
 		return lipgloss.JoinHorizontal(
 			lipgloss.Left,
-			columnStyle.Render(todoView),
-			focusedStyle.Render(inProgress),
-			columnStyle.Render(done),
+			columnStyle.Render(todoView.View()),
+			focusedStyle.Render(inProgress.View()),
+			columnStyle.Render(done.View()),
 		)
 	default:
+		todoView.SetShowHelp(true)
 		return lipgloss.JoinHorizontal(
 			lipgloss.Left,
-			focusedStyle.Render(todoView),
-			columnStyle.Render(inProgress),
-			columnStyle.Render(done),
+			focusedStyle.Render(todoView.View()),
+			columnStyle.Render(inProgress.View()),
+			columnStyle.Render(done.View()),
 		)
 	}
 
@@ -192,19 +331,23 @@ func (m *App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, cmd
 		}
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl+c":
+		switch {
+		case key.Matches(msg, DefaultKeyMap.Quit):
 			m.quitting = true
 			return m, tea.Quit
-		case "left", "h":
-			m.Prev()
-		case "right", "l":
-			m.Next()
-		case "k", "up":
+		case key.Matches(msg, DefaultKeyMap.Up):
 			m.MoveUp()
-		case "j", "down":
+		case key.Matches(msg, DefaultKeyMap.Down):
 			m.MoveDown()
-		case "e":
+		case key.Matches(msg, DefaultKeyMap.Left):
+			m.Prev()
+		case key.Matches(msg, DefaultKeyMap.Right):
+			m.Next()
+		case key.Matches(msg, DefaultKeyMap.Create):
+			Models[Column] = m
+			Models[form] = NewForm(m.focused)
+			return Models[form].Update(nil)
+		case key.Matches(msg, DefaultKeyMap.Edit):
 			if len(m.lists[m.focused].Items()) == 0 {
 				return m, nil
 			}
@@ -217,23 +360,13 @@ func (m *App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			)
 			m.DeleteTask()
 			return Models[form].Update(nil)
-		case "enter":
-			return m, m.MoveToNext
-		case "n":
-			Models[Column] = m
-			Models[form] = NewForm(m.focused)
-			return Models[form].Update(nil)
-		case "d":
+		case key.Matches(msg, DefaultKeyMap.Delete):
 			if len(m.lists[m.focused].Items()) == 0 {
 				return m, nil
 			}
 			m.DeleteTask()
-		case "s":
-			Models[Column] = m
-			m.showRaw = true
-			d := NewDebug("salve teste", m.lists)
-			return d.Update(nil)
-
+		case key.Matches(msg, DefaultKeyMap.MoveNext):
+			return m, m.MoveToNext
 		default:
 			return m, nil
 		}
